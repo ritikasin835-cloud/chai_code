3d2c26b4897b45b22547bf6a5acca1bdd214e624
Ritika singh <ritikasin835@gmail.com>
2025-11-05 19:24:34 +0000

    02_datTypes change configure done

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all js code as newer version
+
+//alert("hello");
+// alert(3+3)//we are using nodejs,not broweser
+console.log(3
+    +3)//code readibity should be high
+console.log("ritika")
+
+let name="ritika"
+let age=19
+let isLogin=false
+
+//types of datatype can be in js number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+    //symbol==>unique
+
+
+//object
+
+console.log(typeof "ritika")
+console.log(age)
+console.log(typeof null)
+console.log(typeof undefined)//output undefined
