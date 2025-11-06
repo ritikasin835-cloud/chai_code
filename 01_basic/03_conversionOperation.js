cdd998b741e9f74577dd1bf69d3e61f153b76948
Ritika singh <ritikasin835@gmail.com>
2025-11-06 09:45:36 +0000

    conversionOperation config done

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,66 @@
+// // let score ="33"
+
+// let score ="33abc"
+
+// // console.log(typeof score)
+
+// // console.log(typeof(score))
+
+// // let valuein=Number(score)
+// // console.log(typeof valuein)
+// // console.log(valuein)
+
+// //"33"=>33 (value)
+// //"33abc"=>NaN but type is number
+// //true=>1 ;
+
+// let isLoggedin="ritika"
+// //""
+// //1
+// //1==>true
+// //""=>false
+// //"ritika"=true
+// let booleanisloogedin=Boolean(isLoggedin)
+// console.log(booleanisloogedin)
+
+
+// let stringNummber=22
+// let number =String(stringNummber)
+// console.log(number);
+// //operations
+
+// console.log(2**4)
+// console.log(2%7)
+
+// let str1="ritika"
+// let str2=" singh"
+
+// let str3 =str1+str2
+// console.log(str3);
+
+// //that is problem
+// console.log("1" + 2+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log(1+2+3+"5");
+// console.log("1" + (2+2));
+console.log(true);
+console.log(+true);
+//console.log(true+);
+//error
+//console.log(+"");
+//error
+
+
+//assignment operator
+let num1,num2,num3
+
+num1=num2=num3=2+3
+//its not readable
+
+
+let gameCounter=100
+++gameCounter;
+//prefix
+//gameCounter++//postfix
+console.log(gameCounter);
